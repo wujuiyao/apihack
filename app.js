64fe7bf2bc7c8404fc32a3934b93ab60d86ab5f0
Richard Wu <contactrichardwu@gmail.com>
2015-09-18 10:45:41 +0800

    test our insta api link

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 $(document).ready(function(){
 
-$('.hash-getter').submit(function(event){
-  // zero out results if previous search has run
-  $('.search-title').html('');
-  var tag = $(this).find('input[name="hashtag"]').val();
-  console.log(tag);
-});
+  //variables
+  var clientID = "155b290399484dd39e1dda80c3864fc1";
 
-var clientID = "155b290399484dd39e1dda80c3864fc1";
-var clientSecret = "e9e28792ec2b4555be01a2222003a290";
-var url = "https://api.instagram.com/v1/tags/nofilter/media/recent?client_id=CLIENT-ID";
+  /* Event Handlers */
+  //Search Input
+  $('.hash-getter').submit(function(event){
+    // zero out results if previous search has run
+    $('.search-title').html('');
+    var tag = $(this).find('input[name="hashtag"]').val();
+    showSearchTitle(tag);
+    getResult(tag);
+  });
 
-var mostLikedHash = $ajax({
 
-});
+  function showSearchTitle(tag){
+    console.log(tag);
+    $('.search-title').html(tag);
+  }
+
+  //get the auth right
+
+  function getResult(tag){
+
+    var result = $.ajax({
+      type: 'GET',
+      dataType: 'jsonp',
+      url : 'https://api.instagram.com/v1/tags/tag/media/recent?client_id=155b290399484dd39e1dda80c3864fc1'
+    })
+    .done(function(result){
+      console.log(result);
+    });
+  }
 
 });
